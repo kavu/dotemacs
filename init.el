46186a7af3d9370eec50ad166cec09230430c2c7
Max Riveiro <kavu13@gmail.com>
2010-06-23 02:13:28 +0400

    Added autopair, enabled only for ruby-mode

--- init.el
+++ init.el
@@ -250,3 +250,8 @@
 (add-to-list 'load-path "~/.emacs.d/packages/zencoding")
 (require 'zencoding-mode)
     (add-hook 'sgml-mode-hook 'zencoding-mode)
+
+(require 'autopair)
+;; (autopair-global-mode)
+(add-hook 'ruby-mode-hook 
+	  '(lambda () (autopair-mode)))
