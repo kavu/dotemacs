60e7dddbfef9a44ea3a2f76ff5a04d208247e4c7
Max Riveiro <kavu13@gmail.com>
2010-07-23 00:02:34 +0400

    Added haskell-mode

--- init.el
+++ init.el
@@ -293,6 +293,11 @@
 (add-hook 'css-mode-hook 'rainbow-mode)
 (add-hook 'sass-mode-hook 'rainbow-mode)
 
+(add-to-list 'load-path "~/.emacs.d/packages/haskell-mode")
+(load "haskell-site-file")
+(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+
 (custom-set-variables
  '(speedbar-indentation-width 3)
  '(speedbar-smart-directory-expand-flag nil)
