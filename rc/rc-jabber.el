b07fdf67c810aba72522ae16980c80e874b2048a
Max Riveiro <kavu13@gmail.com>
2010-08-27 21:02:53 +0400

    Jabber.el message notifications via notify.el

--- rc/rc-jabber.el
+++ rc/rc-jabber.el
@@ -1,8 +1,18 @@
 (require 'jabber-autoloads)
+(require 'notify)
 
 (setq jabber-account-list
  '(("kavu13@gmail.com/emacs"
    (:network-server . "talk.google.com")
    (:connection-type . ssl))))
+
+(defun libnotify-hook (from buffer text alert)
+  (when (or jabber-message-alert-same-buffer
+            (not (memq (selected-window) (get-buffer-window-list buffer))))
+    (notify (if (jabber-muc-sender-p from)
+                (jabber-jid-displayname (jabber-jid-user from))
+              (jabber-jid-displayname from)) text)))
+
+(add-hook 'jabber-alert-message-hooks 'libnotify-hook)
 
 (setq fsm-debug nil)
