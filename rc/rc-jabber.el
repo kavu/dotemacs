8e6de79ff29f3c1c2dca9b8997530bb36f1a890f
Max Riveiro <kavu13@gmail.com>
2010-08-27 21:03:20 +0400

    Experimental jabber.el message notification for Awesome WM

--- rc/rc-jabber.el
+++ rc/rc-jabber.el
@@ -6,6 +6,13 @@
    (:network-server . "talk.google.com")
    (:connection-type . ssl))))
 
+;; (defun awesome-notify (text title)
+;;   (condition-case e
+;;       (let ((process-connection-type))
+;;         (shell-command-to-string (format "echo 'naughty.notify({text = \"%s\", title = \"%s\" %s})' | awesome-client -"
+;;                                          text title jabber-awesome-args)))
+;;     (error nil)))
+
 (defun libnotify-hook (from buffer text alert)
   (when (or jabber-message-alert-same-buffer
             (not (memq (selected-window) (get-buffer-window-list buffer))))
